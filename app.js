993d501453c1b756d90c7a8f4f392432fb044b8c
Thiago González <thiagogonzalez2000@gmail.com>
2021-11-01 16:49:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 
-mongoose.connect("mongodb://localhost:27017/blogDB");
+mongoose.connect(");
 
 const postsSchema = {
   title: String,
